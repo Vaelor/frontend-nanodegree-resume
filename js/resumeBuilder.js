724e163335b2214c717976ffe36711c9e7f63d5f
Christian Pluemer <christian.pluemer@auerswald.de>
2015-12-09 14:44:22 +0100

    Safekeeping/sync between devices

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+$(document).ready(function() {
+	console.log( "ready!" );
+	console.log(work);
+	work.showWork();
+});
+
+function showWork() {
+	console.log('showWork');
+}
